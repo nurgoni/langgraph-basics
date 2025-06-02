81d73e3e049e759dc73d6b26f2fa70c0a0e17467
AnnurAfgoni <afgoniannur@gmail.com>
2025-06-02 16:49:34 +0800

    feat: add simple chatbot agents

---
+++ src/agents/chatbot.py
@@ -0,0 +1,43 @@
+from langgraph.graph import START, END, StateGraph
+from typing import Annotated, TypedDict
+from langgraph.graph.message import add_messages
+
+from core import get_llm, settings
+
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+
+class ChatBot:
+    def __init__(self):
+        """
+        
+        """
+        self.model = get_llm(settings.DEFAULT_MODEL)
+
+    def graph_builder(self):
+        """
+        
+        """
+        builder = StateGraph(State)
+
+        builder.add_node("chatbot", self.chat)
+        builder.add_edge(START, "chatbot")
+        builder.add_edge("chatbot", END)
+
+        return builder
+
+    def chat(self, state: State) -> State:
+        """
+        
+        """
+        response = self.model.invoke(state["messages"])
+        return {
+            "messages": [response]
+        }
+
+
+if __name__ == "__main__":
+    chatbot = ChatBot()
+    chatbot()
